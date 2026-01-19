182d591fc0da98330ba9346654f16a7dd4abbeaf
lavi <165945697+scriptlver@users.noreply.github.com>
2026-01-19 19:44:53 -0300

    foi mores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,26 @@
+// FAVORITOS
 document.querySelectorAll('.fav-btn').forEach(btn => {
   btn.addEventListener('click', () => {
     const img = btn.querySelector('img');
 
     btn.classList.toggle('active');
 
+    const basePath = location.pathname.includes('/songs/')
+      ? '../'
+      : '';
+
     img.src = btn.classList.contains('active')
-      ? '../assets/images/icons/favorite.png'
-      : '../assets/images/icons/desfavorite.png';
+      ? `${basePath}assets/images/icons/favorite.png`
+      : `${basePath}assets/images/icons/desfavorite.png`;
   });
 });
 
-const isInsideFolder = window.location.pathname.includes('/songs/');
+// MENU MOBILE
+const basePath = location.pathname.includes('/songs/')
+  ? '../'
+  : '';
 
-const menuPath = isInsideFolder
-  ? '../menu-mobile.html'
-  : 'menu-mobile.html';
-
-fetch(menuPath)
+fetch(`${basePath}menu-mobile.html`)
   .then(res => res.text())
   .then(html => {
     document.getElementById('menu-mobile').innerHTML = html;
