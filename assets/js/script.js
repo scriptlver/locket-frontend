35494b895d25176049b273a70849500650bb50cb
lavi <165945697+scriptlver@users.noreply.github.com>
2026-01-19 19:25:31 -0300

    salvando dnv pra nn dar mais problema

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,31 @@ document.querySelectorAll('.fav-btn').forEach(btn => {
     btn.classList.toggle('active');
 
     img.src = btn.classList.contains('active')
-      ? 'assets/images/icons/favorite.png'
-      : 'assets/images/icons/desfavorite.png';
+      ? '../assets/images/icons/favorite.png'
+      : '../assets/images/icons/desfavorite.png';
   });
 });
 
-fetch("menu-mobile.html")
+const isInsideFolder = window.location.pathname.includes('/songs/');
+
+const menuPath = isInsideFolder
+  ? '../menu-mobile.html'
+  : 'menu-mobile.html';
+
+fetch(menuPath)
   .then(res => res.text())
   .then(html => {
-    document.getElementById("menu-mobile").innerHTML = html;
+    document.getElementById('menu-mobile').innerHTML = html;
 
-    const menu = document.getElementById("menu");
-    const openBtn = document.querySelector(".menu-icon");
-    const closeBtn = document.getElementById("closeMenu");
+    const menu = document.getElementById('menu');
+    const openBtn = document.querySelector('.menu-icon');
+    const closeBtn = document.getElementById('closeMenu');
 
-    openBtn.addEventListener("click", () => {
-      menu.classList.add("active");
+    openBtn.addEventListener('click', () => {
+      menu.classList.add('active');
     });
 
-    closeBtn.addEventListener("click", () => {
-      menu.classList.remove("active");
+    closeBtn.addEventListener('click', () => {
+      menu.classList.remove('active');
     });
   });
